7ed003a2e855a895c14364eb7c70b5b649be4109
paolaCaushi <pcaushi@unitsystem.net>
2022-11-04 17:04:03 +0100

    finished components

--- src/components/Citycard.vue
+++ src/components/Citycard.vue
@@ -10,8 +10,25 @@
      <p class="text-3xl self-end">
      {{Math.round(city.weater.main.temp)}} &deg;
      </p>
-     
+     <div class="flex gap-2">
+     <span class="text-xs">
+     H:
+     {{Math.round(city.weather.main.temp_max)}} &deg;
+     </span>
+     <span class="text-xs">
+     L:
+     {{Math.round(city.weather.main.temp_min)}} &deg;
+     </span>
+     </div>
      </div>
     </div>
-
 </template>
+
+<script setup>
+defineProps({
+    city:{
+        type:Object,
+        default: () =>{},
+    },
+});
+</script>
